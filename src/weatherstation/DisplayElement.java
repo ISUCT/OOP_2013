5f5d346a6e6f2f65f3d2a1133e36d066d2cc7384
Semina_Y_N <lenatim25@gmail.com>
2013-10-22 19:34:59 +0400

    WEATHERSTATION

---
+++ src/weatherstation/DisplayElement.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherstation;
+
+/**
+ *
+ * @author Юлия
+ */
+public interface DisplayElement {
+    public void display ();
+        
+    }
