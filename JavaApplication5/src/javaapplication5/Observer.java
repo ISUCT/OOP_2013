21883a6083a65697fb9350010cef94b9ffb8fa75
Дашка1 <1@Dashyla-PC>
2013-10-27 20:29:29 +0400

    лаба 3

---
+++ JavaApplication5/src/javaapplication5/Observer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Дашка1
+ */
+public interface Observer {
+  public void update (float temp, float humidity, float pressure);
+
+}
