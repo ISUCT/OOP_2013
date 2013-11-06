f461957a80f2bfaa454cd10fc9055b43557662ac
Bessonova_T_M <tanetk@mail.ru>
2013-11-06 19:13:15 +0400

    Starbuzz

---
+++ src/starbuzzCoffee/CondimentDecorator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starbuzzCoffee;
+
+/**
+ *
+ * @author stud_6
+ */
+public abstract  class CondimentDecorator extends Beverage {
+    public abstract String getDescription () ;
+}
