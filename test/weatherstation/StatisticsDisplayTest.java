37cf6c5fe793a950efffb96378d44b7f474850bf
MatveevaAnn <tanetk@mail.ru>
2013-11-09 23:43:47 +0400

    WeatherStation

---
+++ test/weatherstation/StatisticsDisplayTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherstation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class StatisticsDisplayTest {
+    
+    public StatisticsDisplayTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of update method, of class StatisticsDisplay.
+     */
+ 
+    @Test
+    public void testDisplay() {
+        System.out.println("display");
+        StatisticsDisplay instance = new StatisticsDisplay();
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        // IMPORTANT: Save the old System.out!
+        //PrintStream old = System.out;
+        // Tell Java to use your special stream
+        System.setOut(ps);
+        instance.display();
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals("Вывод статистики\r\n", baos.toString());
+        
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+}
