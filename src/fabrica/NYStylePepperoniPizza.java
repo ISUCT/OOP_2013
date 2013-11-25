577848438efbd30ffe17161cdd771e59658ff63e
Semina_Y_N <lenatim25@gmail.com>
2013-11-25 09:48:20 +0400

    Pattern Fabrica

---
+++ src/fabrica/NYStylePepperoniPizza.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabrica;
+
+/**
+ *
+ * @author Юлия
+ */
+public class NYStylePepperoniPizza extends Pizza{
+   public NYStylePepperoniPizza(){
+    name="NY Style Sauce and Cheese Pizza";
+    dough="Thin Crust Dough";
+    sauce="Marinara Sauce";
+    toppings.add("Grated Regiano Cheese");
+    
+         
+}    
+}
